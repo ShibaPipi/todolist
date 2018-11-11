e8d6f5ce1d9ef41e0fa1c2dee0edefa9170fcb9d
ShibaPipi <906262260@qq.com>
2018-11-11 18:46:10 +0800

    test complete

--- src/App.js
+++ src/App.js
@@ -1,39 +1,71 @@
 import React, {Component} from "react";
-import {CSSTransition} from 'react-transition-group';
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
 import "./style.css";
 
 class App extends Component {
     constructor(props, context) {
         super(props, context);
         this.state = {
-            show: true,
+            list: [],
         };
-        this.handleToggle = this.handleToggle.bind(this);
+        this.handleAddItem = this.handleAddItem.bind(this);
     }
 
     render() {
         return (
             <React.Fragment>
-                <CSSTransition
-                    in={this.state.show}
-                    timeout={1000}
-                    classNames="fade"
-                    unmountOnExit
-                    onEntered={(el)=>{el.style.color="blue"}}
-                    appear={true}
-                >
-                    <div>hello</div>
-                </CSSTransition>
-                <button onClick={this.handleToggle}>toggle</button>
+                <TransitionGroup>
+                    {
+                        this.state.list.map((item, index) => {
+                            return (
+                                <CSSTransition key={index} timeout={1000} classNames="fade" unmountOnExit
+                                    onEntered={(el) => {
+                                        el.style.color = "blue"
+                                    }}
+                                    appear={true}
+                                >
+                                    <div>{item}</div>
+                                </CSSTransition>
+                            )
+                        })
+                    }
+                </TransitionGroup>
+                <button onClick={this.handleAddItem}>toggle</button>
             </React.Fragment>
         )
     }
 
-    handleToggle() {
-        this.setState(() => ({
-            show: !this.state.show
-        }))
+    handleAddItem() {
+        this.setState((prevState) => {
+            return {
+                list: [...prevState.list, "item"],
+            }
+        })
     }
 }
 
+{/*<CSSTransition*/
+}
+{/*in={this.state.show}*/
+}
+{/*timeout={1000}*/
+}
+{/*classNames="fade"*/
+}
+{/*unmountOnExit*/
+}
+{/*onEntered={(el) => {*/
+}
+{/*el.style.color = "blue"*/
+}
+{/*}}*/
+}
+{/*appear={true}*/
+}
+{/*>*/
+}
+{/*<div>hello</div>*/
+}
+{/*</CSSTransition>*/
+}
 export default App;
