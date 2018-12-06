25c6fc2c1074e85e07067ce7876a1d7500c84cfb
ShibaPipi <906262260@qq.com>
2018-12-06 21:17:24 +0800

    redux-thunk finished

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,8 @@
 import React, {Component} from "react";
 import 'antd/dist/antd.css';
-import {Input, Button, List} from 'antd';
 import store from "./store"; // 等价于下一行
-import {getAddItemAction, getDeleteItemAction, getInputChangeAction} from "./store/actionCreators";
+import {getAddItemAction, getDeleteItemAction, getInputChangeAction, getTodoList} from "./store/actionCreators";
+import TodoListUI from "./TodoListUI";
 
 // import store from "./store/index";
 
@@ -14,30 +14,23 @@ class TodoList extends Component {
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleStoreChange = this.handleStoreChange.bind(this);
         this.handleButtonClick = this.handleButtonClick.bind(this);
-        store.subscribe(this.handleStoreChange)
+        this.handleItemClick = this.handleItemClick.bind(this);
+        store.subscribe(this.handleStoreChange);
     }
 
     render() {
-        return (
-            <div style={{marginTop: "10px", marginLeft: "10px"}}>
-                <div>
-                    <Input
-                        value={this.state.inputValue}
-                        placeholder="todo info"
-                        style={{width: "300px", marginRight: "10px"}}
-                        onChange={this.handleInputChange}
-                    />
-                    <Button type="primary" onClick={this.handleButtonClick}>Submit</Button>
-                </div>
-                <List
-                    style={{marginTop: "10px", width: "300px"}}
-                    bordered
-                    dataSource={this.state.list}
-                    renderItem={(item, index) => (
-                        <List.Item onClick={this.handleItemClick.bind(this, index)}>{item}</List.Item>)}
-                />
-            </div>
-        )
+        return <TodoListUI
+            inputValue={this.state.inputValue}
+            list={this.state.list}
+            handleInputChange={this.handleInputChange}
+            handleButtonClick={this.handleButtonClick}
+            handleItemClick={this.handleItemClick}
+        />
+    }
+
+    componentDidMount() {
+        const action = getTodoList();
+        store.dispatch(action);
     }
 
     handleInputChange(e) {
